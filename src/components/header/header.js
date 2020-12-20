d6a72fd897f6f6781167f0c836d200f5a251349c
Nickdem <nickdem559@inbox.ru>
2020-12-20 13:55:30 +0300

    ссылки в хэдере

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,27 +4,32 @@ import { Link, NavLink } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
+
+  const renderLinks = (links) => {
+    return links.map(({name, url}) => {
+      return (
+        <li className="header__nav-link" key={name}>
+          <NavLink activeClassName="active" to={url}>{name}</NavLink>
+        </li>
+      )
+    })
+  }
+
+  const links = [
+    {name: 'People', url: '/people/'},
+    {name: 'Planets', url: '/planets/'},
+    {name: 'Starships', url: '/starships/'},
+    {name: 'Login', url: '/login'},
+    {name: 'Secret', url: '/secret'}
+  ]
+
   return (
     <div className="header">
       <h3 className="header__nav-link">
         <Link className="header__logo" to="/">StarDB</Link>
       </h3>
       <ul className="header__nav-list">
-        <li className="header__nav-link">
-          <NavLink activeClassName="active" to="/people/">People</NavLink>
-        </li>
-        <li className="header__nav-link">
-          <NavLink activeClassName="active" to="/planets/">Planets</NavLink>
-        </li>
-        <li className="header__nav-link">
-          <NavLink activeClassName="active" to="/starships/">Starships</NavLink>
-        </li>
-        <li className="header__nav-link">
-          <NavLink activeClassName="active" to="/login">Login</NavLink>
-        </li>
-        <li className="header__nav-link">
-          <NavLink activeClassName="active" to="/secret">Secret</NavLink>
-        </li>
+        {renderLinks(links)}
       </ul>
     </div>
   );
